9322c74a5f88d26859bf2c9fde5ce90d53b561e6
Miervaldis42 <31883227+Miervaldis42@users.noreply.github.com>
2020-10-23 19:03:37 +0200

    feat (user_entity) : Entity creation
    * Created an enum for the different roles
    * Created a 'User' entity

---
+++ lesamisdelescalade/src/main/java/com/miervaldis42/climbingwebsite/entity/Role.java
@@ -0,0 +1,21 @@
+package com.miervaldis42.climbingwebsite.entity;
+
+public enum Role {
+	ADMIN("Admin"),
+	MEMBER("Membre"),
+	SUBSCRIBER("Abonné(e)");
+	
+	
+	// Display role name
+	private String roleName;
+	
+	// Constructor
+	Role (String roleName) {
+		this.roleName = roleName;
+	}
+
+	// Method
+	public String getRoleName() {
+		return roleName;
+	}
+}
