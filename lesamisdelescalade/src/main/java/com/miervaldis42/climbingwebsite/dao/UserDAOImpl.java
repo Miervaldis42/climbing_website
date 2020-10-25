dbb304d6bd150397b68b1c8386ab1c76d0d464f0
Miervaldis42 <31883227+Miervaldis42@users.noreply.github.com>
2020-10-25 16:02:14 +0100

    feat (HashPassword) : Hashing implementation
    * Added dependency to be able to hash passwords
    * Created a class to hold hashing method
    * Set hashing before saving user object to the database

--- lesamisdelescalade/src/main/java/com/miervaldis42/climbingwebsite/dao/UserDAOImpl.java
+++ lesamisdelescalade/src/main/java/com/miervaldis42/climbingwebsite/dao/UserDAOImpl.java
@@ -1,31 +1,46 @@
 package com.miervaldis42.climbingwebsite.dao;
 
 // Imports
-import com.miervaldis42.climbingwebsite.entity.User;
-
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.hibernate.SessionFactory;
+import org.apache.commons.codec.binary.Hex;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
+import com.miervaldis42.climbingwebsite.helper.*;
+import com.miervaldis42.climbingwebsite.entity.User;
 
-// DAO implementation
+
+
+/* DAO implementation */
 @Repository
 public class UserDAOImpl implements UserDAO {
+	
+	// Class to use the hashing method
+	HashPassword hasherMachine = new HashPassword();
 	
 	// Inject the session factory
 	@Autowired
 	private SessionFactory sessionFactory;
 
 
+	/*
+	 * Methods for CRU
+	 */
 	@Override
 	public void saveUser(User newUser) {
 		Session currentSession = sessionFactory.getCurrentSession();
 		
+		// Hashing password
+		byte[] hashedBytes = hasherMachine.hashPassword(newUser.getPassword());
+		String hashedPassword = Hex.encodeHexString(hashedBytes);
+	    newUser.setPassword(hashedPassword);
+		
 		currentSession.saveOrUpdate(newUser);
 	}
+	
 	
 	@Override
 	public List<User> getUsers() {
@@ -39,4 +54,5 @@ public class UserDAOImpl implements UserDAO {
 		// Return result
 		return users;
 	}
+
 }
