8e520eaa40efcbc22d31f703f03d9c38e8557cf1
Miervaldis42 <31883227+Miervaldis42@users.noreply.github.com>
2021-01-12 16:20:20 +0100

    feat&fix (MainController) : Searchbar
    * Created a search bar to retrieve sites by name or location
    * Added text when no result for the search
    * Regrouped redundant code in a single method
    * Created a method using user entered keywords for the search in site list page
    * Fixed siteTag position
    * Changed siteTag icon in mobile

--- lesamisdelescalade/src/main/java/com/miervaldis42/climbingwebsite/controller/MainController.java
+++ lesamisdelescalade/src/main/java/com/miervaldis42/climbingwebsite/controller/MainController.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import javax.servlet.http.HttpSession;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 import java.util.Map;
 
@@ -34,14 +37,30 @@ public class MainController {
 		
 		
 		// Populate model to use for site cards
-		List<Site> allSites = siteService.getSites();
-		Map<Integer, List<Integer>> siteCardsInfo = siteService.getSiteCards(allSites);
-		Map<Integer, String> siteQuotation = Difficulty.EASY.getQuotation(siteCardsInfo);
-		cards.addAttribute("sites", allSites);
-		cards.addAttribute("infos", siteCardsInfo);
-		cards.addAttribute("quotation", siteQuotation);
+		List<Site> sites = siteService.getSites();
+		setSiteCardInfo(sites, cards);		
 		
 		return "siteList-page";
+	}
+	
+	
+	@GetMapping("/search")
+    public String searchSites(@RequestParam("searchedTerms") String searchedTerms, Model cards) {
+		List<Site> searchedSites = siteService.searchSites(searchedTerms);
+		setSiteCardInfo(searchedSites, cards);
+		
+        return "siteList-page";        
+    }
+	
+	
+	// Set info for each site card
+	private void setSiteCardInfo(List<Site> sites, Model cards) {
+		Map<Integer, List<Integer>> siteCardsInfo = siteService.getSiteCards(sites);		
+		Map<Integer, String> siteQuotation = Difficulty.EASY.getQuotation(siteCardsInfo);
+		
+		cards.addAttribute("sites", sites);
+		cards.addAttribute("infos", siteCardsInfo);
+		cards.addAttribute("quotation", siteQuotation);
 	}
 
 }
